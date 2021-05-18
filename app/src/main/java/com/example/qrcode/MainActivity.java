35217c61323faf8635942c0732ea83f702d435ed
MrpTERO <v.gostax.u.frosta@gmail.com>
2021-05-18 09:46:48 +0300

    as

---
+++ app/src/main/java/com/example/qrcode/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.qrcode;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    ProgressBar progressBar;
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        webView = (WebView) findViewById(R.id.webView);
+        webView.setWebViewClient(new myWebClient());
+        webView.setWebChromeClient(new WebChromeClient(){
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                progressBar.setProgress(newProgress);
+                if(newProgress==100)
+                    progressBar.setVisibility(View.GONE);
+                else
+                    progressBar.setVisibility(View.VISIBLE);
+            }
+        });
+
+        WebSettings webset = webView.getSettings();
+        webset.setJavaScriptEnabled(true);
+        webView.loadUrl("https://tahvel.edu.ee/#/schoolBoard/14/group/3074");
+    }
+}
---
+++ app/src/main/java/com/example/qrcode/myWebClient.java
@@ -0,0 +1,16 @@
+package com.example.qrcode;
+
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+public class myWebClient extends WebViewClient {
+
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+        view.loadUrl(url);
+        return true;
+    }
+}
